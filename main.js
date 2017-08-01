6b346fcd93b09002002efd1dfb11bfb28d690363
fusionx1 <paul.depaula@gmail.com>
2017-08-01 10:14:29 +0800

    changes images folder directory

--- main.js
+++ main.js
@@ -27,22 +27,22 @@ async function navigateWebsite() {
   ///////////////////////////////
   // Navigate to a onelogin page
   //////////////////////////////
-  await mainTab.goTo('https://pantheon.onelogin.com/login')
-  await mainTab.fill('#user_email', 'xxxxxxxxx')
+  await mainTab.goTo('xxxxxxxxxxxx')
+  await mainTab.fill('#user_email', 'paul.depaula@pantheon.io')
   // Type in an element
-  await mainTab.fill('#user_password', 'xxxxxxxxx')
+  await mainTab.fill('#user_password', 'xxxxxxxxxxxxx')
   await mainTab.click('#user_submit')
   await mainTab.log('Click login')
   await mainTab.log('Wait 5 seconds')
   await mainTab.wait(5000)
-  await mainTab.saveScreenshot('./onelogin.png')
+  await mainTab.saveScreenshot('screenshots/onelogin.png')
   await mainTab.log('Took a screenshot of the onelogin form')
   await mainTab.fill("input[name='otp_token_1']", yubi_key)
   // Just added yubi key
   await mainTab.click('#user_submit')
   await mainTab.log('Wait 5 seconds')
   await mainTab.wait(5000)
-  await mainTab.saveScreenshot('./added-yubi.png')
+  await mainTab.saveScreenshot('screenshots/added-yubi.png')
   await mainTab.log('Took a screenshot of the onelogin form with yubikey')
   // Click on a button
   await mainTab.click('#browser_submit')
@@ -55,7 +55,7 @@ async function navigateWebsite() {
   await mainTab.log('Finally logging in successfully to one login')
   await mainTab.wait(5000)
   await mainTab.log('wait 5s.')
-  await mainTab.saveScreenshot('./onelogin-dashboard')
+  await mainTab.saveScreenshot('screenshots/onelogin-dashboard')
   await mainTab.goTo('https://pantheon.onelogin.com/client/apps/select/73297966')
   await mainTab.log('clicked to authenticate pantheon admin dashoard')
 
@@ -69,7 +69,7 @@ async function navigateWebsite() {
   await  mainTab.log('wait 5s..')
   await  mainTab.wait(5000)
   await  mainTab.log('wait 5s...')
-  await  mainTab.saveScreenshot('./pantheon-admin-dashboard.png')
+  await  mainTab.saveScreenshot('screenshots/pantheon-admin-dashboard.png')
   await  mainTab.log('screenshot pantheon admin dashboard')
   await  mainTab.goTo('https://admin.dashboard.pantheon.io/sites/' + site_uuid + '#live/new_relic/NewRelic')
   await  mainTab.log('navigate to the site where audit will take place111111111111')
@@ -83,7 +83,7 @@ async function navigateWebsite() {
   await  mainTab.log('wait 5s....')
   await  mainTab.wait(5000)
   await  mainTab.log('wait 5s.....')
-  await  mainTab.saveScreenshot('./site-dashboard-tobe-audited.png')
+  await  mainTab.saveScreenshot('screenshots/site-dashboard-tobe-audited.png')
   await  mainTab.log('navigate to the site where audit will take place1a')
   await  mainTab.click('#sso-form > button')
   await  mainTab.log('navigate to the site where audit will take place1b')
@@ -98,27 +98,29 @@ async function navigateWebsite() {
   const secondTab = await browser.newTab({
     privateTab: false // Optional. Defaults to false
   })
-  
-  await secondTab.saveScreenshot('./nr-overview.png')
+
+  await secondTab.saveScreenshot('screenshots/nr-overview.png')
   await secondTab.log('finally landed to newrelic dashboard')
   await secondTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id)
   await secondTab.log('navigate to the site where audit will take place1')
   await secondTab.wait(5000)
   await secondTab.log('wait 5s.')
-  await secondTab.saveScreenshot('./site-dashboard-tobe-audited.png')
+  await secondTab.saveScreenshot('screenshots/site-dashboard-tobe-audited.png')
   await secondTab.wait(5000)
   await secondTab.log('navigate to the site where audit will take place2')
   await secondTab.wait(5000)
   await secondTab.click('#time_window_nav > h4')
   await secondTab.wait(2000)
-  await secondTab.saveScreenshot('./set-7days.png')
+  await secondTab.saveScreenshot('screenshots/set-7days.png')
   await secondTab.click('a[data-open-duration="last_7_days"]')
   await secondTab.wait(1000)
-  await secondTab.saveScreenshot('./set-7days.png')
+  await secondTab.saveScreenshot('screenshots/set-7days.png')
   await secondTab.wait(5000)
-  await secondTab.saveScreenshot('./metrics-7days.png')
+  await secondTab.saveScreenshot('screenshots/metrics-7days.png')
 
 
+  await  mainTab.close()
+  await  secondTab.close()
 
   ///////////////////////////////////////
   // Navigate to New relic dashboard
