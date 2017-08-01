fa28a2bf22db1d6f5190d6709ada6ca97f0f17f1
fusionx1 <paul.depaula@gmail.com>
2017-08-01 10:15:39 +0800

    added sh script to conviently run the script script

---
+++ newrelic_image_capture.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SITE_NAME=$1
+YUBI_KEY=$2
+TERMINUS_PATH=$HOME'/Projects/pantheon/terminus/bin/terminus'
+
+SITE_UUID=$($TERMINUS_PATH site:info $SITE_NAME --format=json | jq .id)
+API_KEY=$($TERMINUS_PATH newrelic-data:info $SITE_NAME.live | jq .application.id)
+NEWRELIC_APP_ID=$($TERMINUS_PATH newrelic-data:info $SITE_NAME.live | jq .nr_id)
+NEWRELIC_ID=$($TERMINUS_PATH newrelic-data:info $SITE_NAME.live | jq .application.id)
+
+echo 'INFO':
+echo 'SITE UUID:'  $SITE_UUID
+echo 'YUBI KEY:'  $YUBI_KEY
+echo 'NEWRELIC ID:'  $NEWRELIC_ID
+echo 'NEWRLIC APP:' $NEWRELIC_APP_ID
+
+node main.js $YUBI_KEY $SITE_UUID $NEWRELIC_ID $NEWRELIC_APP_ID
