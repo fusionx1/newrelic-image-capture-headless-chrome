26b5529a64db818e4927dfe48f7a577bb2b02075
Paul Jebulan de Paula <paul.depaula@gmail.com>
2017-08-01 23:56:06 +0800

    Update newrelic_image_capture.sh

--- newrelic_image_capture.sh
+++ newrelic_image_capture.sh
@@ -4,9 +4,12 @@ YUBI_KEY=$2
 TERMINUS_PATH=$HOME'/Projects/pantheon/terminus/bin/terminus'
 
 SITE_UUID=$($TERMINUS_PATH site:info $SITE_NAME --format=json | jq .id)
+SITE_UUID="${SITE_UUID%\"}"
+SITE_UUID="${SITE_UUID#\"}"
+
 API_KEY=$($TERMINUS_PATH newrelic-data:info $SITE_NAME.live | jq .application.id)
-NEWRELIC_APP_ID=$($TERMINUS_PATH newrelic-data:info $SITE_NAME.live | jq .nr_id)
-NEWRELIC_ID=$($TERMINUS_PATH newrelic-data:info $SITE_NAME.live | jq .application.id)
+NEWRELIC_ID=$($TERMINUS_PATH newrelic-data:info $SITE_NAME.live | jq .nr_id)
+NEWRELIC_APP_ID=$($TERMINUS_PATH newrelic-data:info $SITE_NAME.live | jq .application.id)
 
 echo 'INFO':
 echo 'SITE UUID:'  $SITE_UUID
