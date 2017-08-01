4f280e6b8bd1ce9af06207e3c47f63eba9eb199e
fusionx1 <paul.depaula@gmail.com>
2017-08-01 23:50:01 +0800

    added shell script to run it using terminus

--- main.js
+++ main.js
@@ -27,10 +27,10 @@ async function navigateWebsite() {
   ///////////////////////////////
   // Navigate to a onelogin page
   //////////////////////////////
-  await mainTab.goTo('xxxxxxxxxxxx')
+  await mainTab.goTo('https://pantheon.onelogin.com/login')
   await mainTab.fill('#user_email', 'paul.depaula@pantheon.io')
   // Type in an element
-  await mainTab.fill('#user_password', 'xxxxxxxxxxxxx')
+  await mainTab.fill('#user_password', 'TGB1234~')
   await mainTab.click('#user_submit')
   await mainTab.log('Click login')
   await mainTab.log('Wait 5 seconds')
@@ -48,6 +48,7 @@ async function navigateWebsite() {
   await mainTab.click('#browser_submit')
   await mainTab.wait(5000)
   await mainTab.log('wait 5s.')
+  await mainTab.waitForPageToLoad()
 
   ///////////////////////////////
   // Navigate to a onelogin page
@@ -55,9 +56,12 @@ async function navigateWebsite() {
   await mainTab.log('Finally logging in successfully to one login')
   await mainTab.wait(5000)
   await mainTab.log('wait 5s.')
+
+
   await mainTab.saveScreenshot('screenshots/onelogin-dashboard')
   await mainTab.goTo('https://pantheon.onelogin.com/client/apps/select/73297966')
   await mainTab.log('clicked to authenticate pantheon admin dashoard')
+  await mainTab.waitForPageToLoad()
 
 
   ///////////////////////////////////////
@@ -72,7 +76,7 @@ async function navigateWebsite() {
   await  mainTab.saveScreenshot('screenshots/pantheon-admin-dashboard.png')
   await  mainTab.log('screenshot pantheon admin dashboard')
   await  mainTab.goTo('https://admin.dashboard.pantheon.io/sites/' + site_uuid + '#live/new_relic/NewRelic')
-  await  mainTab.log('navigate to the site where audit will take place111111111111')
+  await  mainTab.log('navigate to the site where audit will take place')
   await  mainTab.wait(5000)
   await  mainTab.log('wait 5s.')
   await  mainTab.wait(5000)
@@ -84,11 +88,15 @@ async function navigateWebsite() {
   await  mainTab.wait(5000)
   await  mainTab.log('wait 5s.....')
   await  mainTab.saveScreenshot('screenshots/site-dashboard-tobe-audited.png')
-  await  mainTab.log('navigate to the site where audit will take place1a')
-  await  mainTab.click('#sso-form > button')
-  await  mainTab.log('navigate to the site where audit will take place1b')
   await  mainTab.wait(5000)
-
+  await  mainTab.log('wait 5s...')
+  await  mainTab.wait(5000)
+  await  mainTab.log('wait 5s....')
+  await  mainTab.wait(5000)
+  await  mainTab.log('wait 5s.....')
+  await  mainTab.click('#sso-form > button')
+  await  mainTab.log('Authenticate to newrelic SSO')
+  await  mainTab.wait(5000)
 
 
   ///////////////////////////////////////
@@ -104,23 +112,85 @@ async function navigateWebsite() {
   await secondTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id)
   await secondTab.log('navigate to the site where audit will take place1')
   await secondTab.wait(5000)
-  await secondTab.log('wait 5s.')
-  await secondTab.saveScreenshot('screenshots/site-dashboard-tobe-audited.png')
-  await secondTab.wait(5000)
-  await secondTab.log('navigate to the site where audit will take place2')
-  await secondTab.wait(5000)
+
+  //const downtimeClass = "a[href='/accounts/" + newrelic_id + '/applications/' + newrelic_app_id + "/downtime']"
+
   await secondTab.click('#time_window_nav > h4')
   await secondTab.wait(2000)
   await secondTab.saveScreenshot('screenshots/set-7days.png')
   await secondTab.click('a[data-open-duration="last_7_days"]')
-  await secondTab.wait(1000)
-  await secondTab.saveScreenshot('screenshots/set-7days.png')
   await secondTab.wait(5000)
   await secondTab.saveScreenshot('screenshots/metrics-7days.png')
 
+  await secondTab.click('#time_window_nav > h4')
+  await secondTab.wait(2000)
+  await secondTab.click('a[data-open-duration="last_90_days"]')
+  await secondTab.wait(5000)
+  await secondTab.saveScreenshot('screenshots/metrics-90days.png')
 
-  await  mainTab.close()
-  await  secondTab.close()
+  const thirdTab = await browser.newTab({
+    privateTab: false // Optional. Defaults to false
+  })
+
+  await thirdTab.log('getting transactions metrics')
+  await thirdTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id +'/transactions')
+  await thirdTab.wait(5000)
+  await thirdTab.saveScreenshot('screenshots/nr-transactions')
+
+  const fourthTab = await browser.newTab({
+    privateTab: false // Optional. Defaults to false
+  })
+
+  await fourthTab.log('getting hooks metrics')
+  await fourthTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id +'/hooks')
+  await fourthTab.wait(5000)
+  await fourthTab.saveScreenshot('screenshots/nr-hooks')
+
+  const fifthTab = await browser.newTab({
+    privateTab: false // Optional. Defaults to false
+  })
+
+  await fifthTab.log('getting themes/plugins metrics')
+  await fifthTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id +'/wordpress/plugins')
+  await fifthTab.wait(5000)
+  await fifthTab.saveScreenshot('screenshots/nr-themes-plugins')
+
+  const sixthTab = await browser.newTab({
+    privateTab: false // Optional. Defaults to false
+  })
+
+  await sixthTab.log('getting database metrics')
+  await sixthTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id +'/datastores')
+  await sixthTab.wait(5000)
+  await sixthTab.saveScreenshot('screenshots/nr-database')
+
+  const seventhTab = await browser.newTab({
+    privateTab: false // Optional. Defaults to false
+  })
+
+  await seventhTab.log('getting external calls metrics')
+  await seventhTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id +'/externals')
+  await seventhTab.wait(5000)
+  await seventhTab.saveScreenshot('screenshots/nr-external-calls')
+
+  const eightTab = await browser.newTab({
+    privateTab: false // Optional. Defaults to false
+  })
+
+  await eightTab.log('getting sla 3months metrics')
+  await eightTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id +'/optimize/sla_report')
+  await eightTab.wait(5000)
+  await eightTab.saveScreenshot('screenshots/nr-3month-sla')
+
+  const nighthTab = await browser.newTab({
+    privateTab: false // Optional. Defaults to false
+  })
+
+  await nighthTab.log('getting sla 12months metrics')
+  await nighthTab.goTo('https://rpm.newrelic.com/accounts/' + newrelic_id + '/applications/' + newrelic_app_id +'/optimize/sla_report?interval=months')
+  await nighthTab.wait(5000)
+  await nighthTab.saveScreenshot('screenshots/nr-12month-sla')
+
 
   ///////////////////////////////////////
   // Navigate to New relic dashboard
